cd1266c281e43164f5d5a2afc100ab2e5bc236ff
Ian Lim <limik@lopers.unk.edu>
2017-01-31 11:32:43 -0600

    Added comments

--- src/CellThread.java
+++ src/CellThread.java
@@ -17,10 +17,6 @@ public class CellThread implements Callable<List<Point>> {
     private int width;
     HashMap<Integer, CellThread> prevGen;
 
-//    private boolean alive;
-
-    //    GameMap gameMap;
-
     public CellThread(int x, int y, int height, int width, HashMap<Integer, CellThread> prevGen)
     {
         this.x = x;
@@ -31,21 +27,26 @@ public class CellThread implements Callable<List<Point>> {
     }
 
     /**
-     * cool stuff, like really cool stuff, blows your mind really cool stuff
-     * @return
+     * Checks if the previous generation is alive or if its hash map is empty
+     * then returns a new list of arrays containing x and y coordinates
+     * @return A list of x and y coordinates
      * @throws Exception
      */
     @Override
     public List<Point> call() throws Exception {
-
         if (alivePrevGen() || prevGen.size() == 0) {
             return returnNeighbors();
         } else {
             return Arrays.asList(new Point(x, y));
-//            return null;
         }
     }
 
+    /**
+     * Checks the surrounding alive cells in the previous generation. Then will return
+     * True if there are only three adjacent live cells, otherwise it will
+     * return false.
+     * @return True if there are 3 adjacent cells alive around the current cell
+     */
     public boolean alivePrevGen() {
         int adjacent = 0;
         // 1 2 3
@@ -115,54 +116,10 @@ public class CellThread implements Callable<List<Point>> {
         }
     }
 
-//    public List<CellThread> returnNeighbors() {
-//        LinkedList<CellThread> result = new LinkedList();
-//        // 1 2 3
-//        // 4 X 5
-//        // 6 7 8
-//        if (getX() > 0 && getY() > 0) // 1
-//        {
-//            result.add(prevGen.get((getX()-1) + (getY()-1) *10));
-////            result.add(getCell(gen, getX() - 1, getY() - 1));
-//        }
-//        if (getX() > 0) // 2
-//        {
-//            result.add(prevGen.get((getX() - 1)+ getY() * 10));
-////            result.add(getCell(gen, getX() - 1, getY()));
-//        }
-//        if (getX() > 0 && getY() < height - 1) // 3
-//        {
-//            result.add(prevGen.get(((getX() - 1) + (getY() + 1)*10)));
-////            result.add(getCell(gen, getX() - 1, getY() + 1));
-//        }
-//        if (getY() > 0) // 4
-//        {
-//            result.add(prevGen.get(getX() + (getY() - 1)*10));
-////            result.add(getCell(gen, getX(), getY() - 1));
-//        }
-//        if (getY() < height - 1) // 5
-//        {
-//            result.add(prevGen.get(getX() + (getY() + 1) * 10));
-////            result.add(getCell(gen, getX(), getY() + 1));
-//        }
-//        if (getX() < width - 1 && getY() > 0) // 6
-//        {
-//            result.add(prevGen.get((getX()+1) + (getY() -1) * 10));
-////            result.add(getCell(gen, getX() + 1, getY() - 1));
-//        }
-//        if (getX() < width - 1) // 7
-//        {
-//            result.add(prevGen.get((getX() +1) + getY() * 10));
-////            result.add(getCell(gen, getX() + 1, getY()));
-//        }
-//        if (getX() < width - 1 && getY() < height - 1) // 8
-//        {
-//            result.add(prevGen.get((getX()+1) + (getY() + 1) * 10));
-////            result.add(getCell(gen, getX() + 1, getY() + 1));
-//        }
-//        return result;
-//    }
-
+    /**
+     * Returns a list of neighbors around this current cell
+     * @return List of x and y coordinates
+     */
     public List<Point> returnNeighbors() {
         LinkedList<Point> result = new LinkedList();
         result.add(new Point(x,y));
@@ -220,55 +177,7 @@ public class CellThread implements Callable<List<Point>> {
         return result;
     }
 
-//    public List<Cell> getNeighbors(int gen, Cell c)
-//    {
-//        LinkedList<Cell> result = new LinkedList();
-//        // 1 2 3
-//        // 4 X 5
-//        // 6 7 8
-//        if (c.getX() > 0 && c.getY() > 0) // 1
-//        {
-//            result.add(getCell(gen, c.getX() - 1, c.getY() - 1));
-//        }
-//        if (c.getX() > 0) // 2
-//        {
-//            result.add(getCell(gen, c.getX() - 1, c.getY()));
-//        }
-//        if (c.getX() > 0 && c.getY() < height - 1) // 3
-//        {
-//            result.add(getCell(gen, c.getX() - 1, c.getY() + 1));
-//        }
-//        if (c.getY() > 0) // 4
-//        {
-//            result.add(getCell(gen, c.getX(), c.getY() - 1));
-//        }
-//        if (c.getY() < height - 1) // 5
-//        {
-//            result.add(getCell(gen, c.getX(), c.getY() + 1));
-//        }
-//        if (c.getX() < width - 1 && c.getY() > 0) // 6
-//        {
-//            result.add(getCell(gen, c.getX() + 1, c.getY() - 1));
-//        }
-//        if (c.getX() < width - 1) // 7
-//        {
-//            result.add(getCell(gen, c.getX() + 1, c.getY()));
-//        }
-//        if (c.getX() < width - 1 && c.getY() < height - 1) // 8
-//        {
-//            result.add(getCell(gen, c.getX() + 1, c.getY() + 1));
-//        }
-//        return result;
-//    }
-
-    //    public int getGen() {
-//        return gen;
-//    }
-//
-//    public void setGen(int gen) {
-//        this.gen = gen;
-//    }
-//
+    //============ Getters and Setters ================
     public int getX() {
         return x;
     }
@@ -300,30 +209,7 @@ public class CellThread implements Callable<List<Point>> {
     public void setWidth(int width) {
         this.width = width;
     }
-
-    //
-//    public boolean isAlive() {
-//        return alive;
-//    }
-//
-//    public void setAlive(boolean alive) {
-//        this.alive = alive;
-//    }
-
-//    public CellThread(boolean alive, int x, int y) {
-//        super();
-//        this.alive = alive;
-//        this.x = x;
-//        this.y = y;
-//        gen = 0;
-//        try {
-//            this.wait();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-
+    //============ Getters and Setters ================
 
     @Override
     public String toString()
@@ -339,30 +225,6 @@ public class CellThread implements Callable<List<Point>> {
     {
         String result = "[" + x + "," + y + "]";
         result += "X";
-//        if (this.isAlive())
-//        {
-//        result += "X";
-//        }
-//        else
-//        {
-//            result += "_";
-//        }
         return result;
     }
-//
-//    @Override
-//    public void run() {
-//        try {
-//            alive = false;
-//            synchronized (this) {
-//                this.wait();
-//            }
-//            alive = true;
-//            synchronized (this) {
-//                this.wait();
-//            }
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
